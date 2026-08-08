0c075c91e394fc2b30de157a18d3ed7c82203720
JardisonDev <contdev.io@gmail.com>
2026-08-07 21:53:31 -0400

    Atualizar o snacbar.dart

--- lib/widgets/snacbar.dart
+++ lib/widgets/snacbar.dart
@@ -12,13 +12,7 @@ ShowSnacbar({
       borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
     ),
     duration: Duration(seconds: 3),
-    // action: SnackBarAction(
-    //   label: 'Ok',
-    //   textColor: Colors.white,
-    //   onPressed: () {
-    //     ScaffoldMessenger.of(context).hideCurrentSnackBar();
-    //   },
-    // ),
+    
     elevation: 1,
   );
   ScaffoldMessenger.of(context).showSnackBar(snackBar);
